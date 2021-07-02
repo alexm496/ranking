e118f8bf4f0ca211d219fdcc0c3c0df3c46b1548
SeanStuhlsatz <85950474+SeanStuhlsatz@users.noreply.github.com>
2021-07-02 12:48:16 -0400

    Update Rate_Elo_01.R
    Updated RateElo function to return a matrix of skills at each time rather than just the final skill vector; removed tests

--- Rate_Elo_01.R
+++ Rate_Elo_01.R
@@ -1,22 +1,15 @@
-
-
-RateElo <- function(games,prob,init,k) {
+RateElo <- function(games,prob,init,k,coeff,decay=0) {
   skills <- init
+  skillMat <- matrix(skills,nrow=1,byrow = TRUE)
   for(l in 1:length(games$i)) {
-#    print(games[l,])
-    skills[games$i[l]] <- skills[games$i[l]]+k*(games$results[l]-prob(skills)[games$i[l],games$j[l]])
-#    print(skills)
-    skills[games$j[l]] <- skills[games$j[l]]+k*(-games$results[l]+prob(skills)[games$i[l],games$j[l]])
-#    print(skills)
+    kDecayed <- k/(l^decay)
+    skills[games$i[l]] <- skills[games$i[l]]+
+      kDecayed*(games$results[l]-prob(skills,coeff)[games$i[l],games$j[l]])
+    skills[games$j[l]] <- skills[games$j[l]]+
+      kDecayed*(-games$results[l]+prob(skills,coeff)[games$i[l],games$j[l]])
+    skillMat <- rbind(skillMat,skills)
   }
-  return(skills)
+  return(skillMat)
 }
 
-skillVec <- runif(25,0,15)
-init <- rep(1,25)
-ratings <- RateElo(MakeGames(GetBTProb(skillVec),MatchRandomly(25,10000)),
-        GetBTProb,
-        init,
-        0.5)
-cor(ratings,skillVec)
 
