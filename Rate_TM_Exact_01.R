342b56c2dd8efd02259ebea14d6d49ae31ed017d
SeanStuhlsatz <85950474+SeanStuhlsatz@users.noreply.github.com>
2021-07-07 00:23:58 -0400

    Added the exact Thurstone-Mosteller updates

---
+++ Rate_TM_Exact_01.R
@@ -0,0 +1,36 @@
+RateTMExact <- function(games,priorMeans,priorVar,beta2) {
+  n <- length(games$i)
+  
+  skillMat <- matrix(rep(priorMeans,n+1),nrow=n+1,byrow = TRUE)
+  varMat <- matrix(rep(priorVar,n+1),nrow=n+1,byrow=TRUE)
+
+  for (l in 1:length(games[,1])) {
+    if (games$results[l] == 1)  {
+      winner <- games$i[l]
+      loser <- games$j[l]
+    } else {
+      winner <- games$j[l]
+      loser <- games$i [l]
+    }
+    
+    sigmaT2 <- 2*beta2+varMat[l,winner]+varMat[l,loser]
+    sigmaT <- sqrt(sigmaT2)
+    phi <- dnorm((skillMat[l,winner]-skillMat[l,loser])/sqrt(sigmaT2))
+    Phi <- pnorm((skillMat[l,winner]-skillMat[l,loser])/sqrt(sigmaT2))
+    
+    skillMat[l+1,] <- skillMat[l,]
+    varMat[l+1,] <- varMat[l,]
+    
+    skillMat[l+1,winner] <- skillMat[l,winner]+(varMat[l,winner]*phi)/(sigmaT*Phi)
+    skillMat[l+1,loser] <- skillMat[l,loser]-(varMat[l,loser]*phi)/(sigmaT*Phi)
+
+    varMat[l+1,winner] <- varMat[l,winner]+skillMat[l,winner]^2-skillMat[l+1,winner]^2+((phi*varMat[l,winner])/(Phi*sigmaT2^(3/2)))*
+      (varMat[l,winner]*(skillMat[l,winner]+skillMat[l,loser])+2*(2*beta2+varMat[l,loser])*skillMat[l,winner])
+    varMat[l+1,loser] <-  varMat[l,loser] +skillMat[l,loser]^2 -skillMat[l+1,loser]^2 -((phi*varMat[l,loser])/(Phi*sigmaT2^(3/2)))*(2*(varMat[l,winner]+2*beta2)*skillMat[l,loser]+
+       varMat[l,loser]*(skillMat[l,loser]+skillMat[l,winner]))
+  }
+  
+  return(list(skillMat,varMat))
+  
+}
+
