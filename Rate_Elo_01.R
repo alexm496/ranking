4e61e96f44849611c94a139f46c635c888dcde87
SeanStuhlsatz <85950474+SeanStuhlsatz@users.noreply.github.com>
2021-07-26 21:33:56 -0400

    Efficiency improvement
    Whereas before RateElo computed the entire probability matrix while rating each game, now it only computes the relevant probabilities. Other minor improvements were made.

--- Rate_Elo_01.R
+++ Rate_Elo_01.R
@@ -2,27 +2,30 @@
 #' 
 #' @param games Dataframe containing games (1 row each) with
 #' columns for players i and j and a column for the results
-#' @param prob Function to compute the probabilities.
+#' @param PROB Function to compute the probabilities. Should
+#' take in two skills and optionally other parameters and return
+#' a pairwise win/loss probability.
 #' @param init Initial ratings
 #' @param k Learning rate
 #' @param coeff Coefficient. Passed to prob.
 #' @param decay k coefficient decays with time by a factor
-#' of t^decay
+#' of 1/(t^decay)
+#' @param ... Additional parameters to be passed to PROB.
 #' 
 #' @return Matrix, with each row containing ratings as of a
 #' given game.
-RateElo <- function(games,prob,init,k,coeff,decay=0) {
+RateElo <- function(games,PROB,init,k,coeff,decay=0,...) {
   skills <- init
   skillMat <- matrix(skills,nrow=1,byrow = TRUE)
   for(l in 1:length(games$i)) {
     kDecayed <- k/(l^decay)
-    skills[games$i[l]] <- skills[games$i[l]]+
-      kDecayed*(games$results[l]-prob(skills,coeff)[games$i[l],games$j[l]])
-    skills[games$j[l]] <- skills[games$j[l]]+
-      kDecayed*(-games$results[l]+prob(skills,coeff)[games$i[l],games$j[l]])
+    iSkill <- skills[games$i[l]]
+    jSkill <- skills[games$j[l]]
+    p <- PROB(iSkill,jSkill,coeff,...)
+    skills[games$i[l]] <- iSkill+kDecayed*(games$results[l]-p)
+    skills[games$j[l]] <- jSkill+kDecayed*(p-games$results[l])
     skillMat <- rbind(skillMat,skills)
   }
   return(skillMat)
 }
-
 
